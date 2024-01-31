72c686ee8884ae9bc04a0dd7bf4ffbdd7b834ea2
rheeseula <rheeseula@dgsw.hs.kr>
2024-02-01 07:47:23 +0900

    Feat :: Initialized Spring Application (KT)

---
+++ src/main/kotlin/kr/seula/portfolio/PortfolioSpringApplication.kt
@@ -0,0 +1,11 @@
+package kr.seula.portfolio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PortfolioSpringApplication
+
+fun main(args: Array<String>) {
+    runApplication<PortfolioSpringApplication>(*args)
+}
---
+++ src/test/kotlin/kr/seula/portfolio/PortfolioSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.seula.portfolio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PortfolioSpringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
